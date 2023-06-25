641576e68dad62af342a10b18ce588a26bd93f93
Fernando Link <fernandolink@gmail.com>
2023-06-24 22:15:41 -0300

    Injeção de dependência

--- pep8/fila_normal.py
+++ pep8/fila_normal.py
@@ -1,5 +1,11 @@
+from typing import Union
 from fila_base import FilaBase
 from constantes import CODIGO_NORMAL
+from estatistica_resumida import EstatisticaResumida
+from estatistica_detalhada import EstatisticaDetalhada
+
+
+Classes = Union[EstatisticaResumida, EstatisticaDetalhada]
 
 
 class FilaNormal(FilaBase):
@@ -10,3 +16,6 @@ class FilaNormal(FilaBase):
         cliente_atual: str = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return f'Cliente atual: {cliente_atual}, dirija-se ao caixa {caixa}'
+
+    def estatistica(self, retorna_estatistica: Classes) -> dict:
+        return retorna_estatistica.roda_estatistica(self.clientes_atendidos)
--- pep8/main.py
+++ pep8/main.py
@@ -1,4 +1,7 @@
 from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
+
 
 fila_teste = FabricaFila.pega_fila('normal')
 fila_teste.atualiza_fila()
@@ -7,6 +10,7 @@ fila_teste.atualiza_fila()
 print(fila_teste.chama_cliente(5))
 print(fila_teste.chama_cliente(10))
 print(fila_teste.chama_cliente(6))
+print(fila_teste.estatistica(EstatisticaResumida('24/06/2023', 120)))
 
 fila_teste_prioritaria = FabricaFila.pega_fila('prioritaria')
 fila_teste_prioritaria.atualiza_fila()
@@ -15,4 +19,6 @@ fila_teste_prioritaria.atualiza_fila()
 print(fila_teste_prioritaria.chama_cliente(5))
 print(fila_teste_prioritaria.chama_cliente(10))
 print(fila_teste_prioritaria.chama_cliente(7))
-print(fila_teste_prioritaria.estatistica('10/01/2023', 198, 'detail'))
+print(fila_teste_prioritaria.estatistica(
+    EstatisticaDetalhada('24/06/2023', 120)
+))
